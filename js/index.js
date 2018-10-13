c2b620cd3cbaa8ae1a4a680286b47b219a63ba2b
dyt0229 <2710112390@qq.com>
2018-10-13 09:25:26 +0800

    这是V1.0版本

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// $(function (function titleshow() {
+//     $(".country").click(function () {
+//         $(".ulCountry").css({"display":"block"});
+//         $(".ulCountry").slideDown(1000);
+//     })
+// }))
+
+
+// var tit =  document.getElementById("tit");
+// var tit_divs = tit.getElementsByTagName("div");
+// let t=false;
+// for(var i = 0; i < tit_divs.length; i++){
+//     tit_divs[i].onclick = function(){
+//         this.children[1].style.borderTop = "2px solid #fe5f38";
+//         this.children[1].style.padding = "10px 10px 15px";
+//         this.children[1].style.height = "67px";
+//         t= !t;                                
+//     }
+// }
+
+
